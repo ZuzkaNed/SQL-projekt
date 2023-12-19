1b2444ed0616c8056a686026b0b0f3666d4a3ad4
zuzka <zuzka@nedved.org>
2023-12-19 13:36:04 +0100

    secondary table - table created!

--- projekt_SQL_secondary_final.sql
+++ projekt_SQL_secondary_final.sql
@@ -1,18 +1,20 @@
 
 -- evropské státy a ich populace, GDP a gini koeficient v letech 2006 - 2018 --  
 
-SELECT 
- 	country,
-	`year` ,
-	GDP,
-	gini,
-	population
-FROM economies
-WHERE `year` BETWEEN 2006 AND 2018 
-AND  country  IN 
-		(SELECT 
- 		country
- 		FROM countries
-		WHERE continent LIKE 'Europe')
-ORDER BY country, `year` ;
-		
+CREATE TABLE t_zuzana_nedvedova_project_sql_secondary_final AS 
+	(SELECT 
+ 		country,
+		`year` ,
+		GDP,
+		gini,
+		population
+	FROM economies
+	WHERE `year` BETWEEN 2006 AND 2018 
+	AND  country  IN 
+			(SELECT 
+ 			country
+ 			FROM countries
+			WHERE continent LIKE 'Europe')
+	ORDER BY country, `year` )
+;
+	
