a57a8a397562dbb066eeca0446c7eb92f0dacbbe
zuzka <zuzka@nedved.org>
2023-12-21 09:35:06 +0100

    created table for research queston 1 - average payroll per industry and year

---
+++ research_question1.sql
@@ -0,0 +1,14 @@
+-- 1. Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají? --
+
+-- průměrná mzda (přepočtená na plný uvazek) za každé odvětví od roku 2000 do 2021 --
+
+SELECT 
+	payroll_year AS reference_period,
+	cp.industry_branch_code,
+	ib.name AS industry_name,
+	round(avg(value),0) AS average_payroll_in_czk	
+FROM czechia_payroll cp
+JOIN czechia_payroll_industry_branch ib
+ON cp.industry_branch_code = ib.code 
+WHERE value_type_code = 5958 AND calculation_code = 200
+GROUP BY industry_branch_code, payroll_year;  
